cd1c2e656757c2286c36ba098e4d9dd1bab2d18f
Zebin He <96441701+osnarty@users.noreply.github.com>
2022-10-03 17:45:08 -0400

    Update threeamigos.sh

--- threeamigos.sh
+++ threeamigos.sh
@@ -2,7 +2,7 @@
 #this script sums the first 3 numbers and subtracts it from the 4 with the subtractmachine script
 #ex. ./threeamigos 10 10 10 15
 #sum= 30
-#difference = 15 / 15 14 13 12 .... to 1
+#difference = 15 , it will countdown from 15 14 13 12 .... to 1
 
 sum=$(($1+$2+$3))
 echo "Sum = $sum"
